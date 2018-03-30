3a3c2b1a29c41ffd7b0311e4a0f0568c826dfe9e
Vidit <vidit-agarwal@users.noreply.github.com>
2018-03-30 13:31:38 +0530

    Curl req php file upload

---
+++ curl_server.php
@@ -0,0 +1,22 @@
+<?php
+
+$url='http://127.0.0.1:8088';
+
+echo "Result -";
+//  Initiate curl
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
+echo 'end' ;
+?>
